2bb389495d5bc22b7e3d490b8c1c8e3e1d37992f
Karolina D <karolina.dike@gmail.com>
2021-04-21 21:46:07 +0200

    Update

--- 10/class10/graph1.py
+++
@@ -1,5 +0,0 @@
-graph = {("home", "school"), ("home", "church"), ("home", "bar"),
-         ("school", "home"), ("school", "hospital"), ("hospital", "bar"),
-
-
-
--- 10/class10/main.py
+++
@@ -1,6 +0,0 @@
-def show(who, more, where):
-
-    print("Konferencja",who,more,where)
-
-
-show(whose_con, more_con, where_con)
